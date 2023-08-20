755d38fc9f5de25eac4b9dd699216c595fd5d916
matkrasinski <mat.krasinski@o2.pl>
2023-08-20 14:57:18 +0200

    Backend integration - submitting new customer

--- vue/src/components/RegisterCustomerComponent.vue
+++ vue/src/components/RegisterCustomerComponent.vue
@@ -12,13 +12,14 @@
         <label for="phoneNumber"> Phone number : </label>
         <input type="text" class="form-control" v-model.trim="phoneNumber" id="phoneNumber" placeholder="Phone number">
 
-        <label for="employee">Employee : </label>
-            <select class="form-select" v-model.trim="employee" id="employee" >
-                <option selected></option>
-                <option v-for="employee in employees" :key="employee">
-                    {{ employee.first_name + " " + employee.last_name }}
-                </option>
+        <label for="employeeId">Employee: </label>
+            <select class="form-select" v-model="employee" id="employeeId">
+            <option value="" disabled>Select an employee</option>
+            <option v-for="employee in employees" :key="employee.id" :value="employee">
+                {{ employee.first_name + " " + employee.last_name }}
+            </option>
             </select>
+        <input class="btn btn-primary col-8" type="button" value="Select" @click="submitNewCustomer"/>
     </div>
 </template>
 
@@ -38,10 +39,22 @@ export default {
     mounted() {
         this.$http.get(`${this.API_URL}/employees`).then(res => {
             this.employees = res.data
-            console.log(this.employees)
         }).catch(error => {
             console.log(error.message);
         });
+    },
+    methods: {
+        submitNewCustomer() {
+            this.$http.post(`${this.API_URL}/customers/new`, {
+                "first_name": this.firstName,
+                "last_name": this.lastName,
+                "email": this.email,
+                "phone_number": this.phoneNumber,
+                "employee_id": this.employee.id
+            }).catch(error => {
+                console.log(error.message)
+            })
+        }
     }
 }
 </script>
